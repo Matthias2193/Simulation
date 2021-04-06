074a5166792ad62d19e7edee291b0fbe764fb2cb
Matthias2193 <matthias.becher2193@gmail.com>
2021-04-06 11:40:59 +0200

    Added function for bivariate simulation.
    Adopted plotting function so it can plot bivariate results

--- Simulation.R
+++ Simulation.R
@@ -71,39 +71,140 @@ uni_sim <- function(n_rep, n_sample, mu, sd, heap_perc, round_mult){
   return(data.frame(cbind(original, rounded, bias, differences)))
 }
 
-plot_results <- function(result_df){
+bi_sim <- function(n_rep, n_sample, mu, sd, heap_perc, round_mult, cor_value){
+  original_1 <- c()
+  original_2 <- c()
+  rounded_1 <- c()
+  rounded_2 <- c()
+  bias_1 <- c()
+  bias_2 <- c()
   
-  plot_data <- data.frame("coefficient" = c(result_df$original,result_df$rounded),y = c(rep("original",nrow(result_df)),rep("rounded",nrow(result_df))))
-  plot_data$y <- as.factor(plot_data$y)
-  p1 <- ggplot(plot_data,aes(x=coefficient, fill=y)) + geom_density(alpha=0.25)
-  show(p1)
+  n_0 <- n_sample[1]
+  n_1 <- n_sample[2]
   
-  t.test(result_df$original, result_df$rounded)
+  m_0 <- mu[1]
+  m_1 <- mu[2]
+  m_2 <- mu[3]
   
+  sd_0 <- sd[1]
+  sd_1 <- sd[2]
+  sd_2 <- sd[3]
   
-  p2 <- ggplot(result_df,aes(x=differences)) + geom_density(color="darkblue", fill="lightblue")
-  show(p2)
+  for(r in seq(1, n_rep)){
+    #Sample x
+    Sig <- (matrix(cor_value, nrow=2, ncol=2) + diag(2)*(1-cor_value)) * sd_1^2
+    data <- data.frame(mvrnorm(n = n_1, mu = c(m_1, m_1), Sigma = Sig))
+    Sig <- (matrix(cor_value, nrow=2, ncol=2) + diag(2)*(1-cor_value)) * sd_1^2
+    data <- rbind(data,data.frame(mvrnorm(n = n_0, mu = c(m_0, m_0), Sigma = Sig)))
+    y <- c(rep(1,n_1),rep(0,n_0))
+    
+    #Create data frame
+    data <- cbind(data,y)
+    
+    #Logistic regression for "true" values
+    model <- glm(y ~., family = binomial, data = data)
+    
+    #Save the coefficient
+    original_1 <- c(original_1,model$coefficients[["X1"]])
+    original_2 <- c(original_2,model$coefficients[["X2"]])
+    #Randomly round 50% of the x values to the nearest 10s
+    for(s in sample(seq(1,(n_1 + n_0)),heap_perc * (n_1+n_0))){
+      data[s,]["X1"] <- round(data[s,]["X1"]/round_mult)*round_mult
+    }
+    
+    #Logistic regression for the rounded values
+    model <- glm(y ~., family = binomial, data = data)
+    
+    #Save the coefficient
+    rounded_1 <- c(rounded_1,model$coefficients[["X1"]])
+    rounded_2 <- c(rounded_2,model$coefficients[["X2"]])
+    
+    bias_1 <- c(bias_1,(original_1[length(original_1)]-rounded_1[length(rounded_1)]) * 100/original_1[length(original_1)])
+    bias_2 <- c(bias_2,(original_2[length(original_2)]-rounded_2[length(rounded_2)]) * 100/original_2[length(original_2)])
+  }
   
-  p3 <- ggplot(result_df,aes(x=bias)) + geom_density(color="darkblue", fill="lightblue")
-  show(p3)
+  differences_1 <- original_1 - rounded_1
+  differences_2 <- original_2 - rounded_2
   
-  fit.norm <- fitdist(result_df$differences, "norm")
-  plot(fit.norm)
-  
-  summary(fit.norm)
+  return(data.frame(cbind(original_1, rounded_1, bias_1, differences_1, original_2, rounded_2, bias_2, differences_2)))
 }
 
+plot_results <- function(result_df, bi = F){
+  if(!bi){
+    plot_data <- data.frame("coefficient" = c(result_df$original,result_df$rounded),y = c(rep("original",nrow(result_df)),rep("rounded",nrow(result_df))))
+    plot_data$y <- as.factor(plot_data$y)
+    p1 <- ggplot(plot_data,aes(x=coefficient, fill=y)) + geom_density(alpha=0.25)
+    show(p1)
+    
+    t.test(result_df$original, result_df$rounded)
+    
+    
+    p2 <- ggplot(result_df,aes(x=differences)) + geom_density(color="darkblue", fill="lightblue")
+    show(p2)
+    
+    p3 <- ggplot(result_df,aes(x=bias)) + geom_density(color="darkblue", fill="lightblue")
+    show(p3)
+    
+    fit.norm <- fitdist(result_df$differences, "norm")
+    plot(fit.norm)
+    
+    summary(fit.norm)
+  } else{
+    
+    plot_data <- data.frame("coefficient" = c(result_df$original_1,result_df$rounded_1),y = c(rep("original",nrow(result_df)),rep("rounded",nrow(result_df))))
+    plot_data$y <- as.factor(plot_data$y)
+    p1 <- ggplot(plot_data,aes(x=coefficient, fill=y)) + geom_density(alpha=0.25) + ggtitle("X1")
+    show(p1)
+    
+    plot_data <- data.frame("coefficient" = c(result_df$original_2,result_df$rounded_2),y = c(rep("original",nrow(result_df)),rep("rounded",nrow(result_df))))
+    plot_data$y <- as.factor(plot_data$y)
+    p1 <- ggplot(plot_data,aes(x=coefficient, fill=y)) + geom_density(alpha=0.25) + ggtitle("X2")
+    show(p1)
+    
+    
+    p2 <- ggplot(result_df,aes(x=differences_1)) + geom_density(color="darkblue", fill="lightblue")
+    show(p2)
+    
+    p2 <- ggplot(result_df,aes(x=differences_2)) + geom_density(color="darkblue", fill="lightblue")
+    show(p2)
+    
+    p3 <- ggplot(result_df,aes(x=bias_1)) + geom_density(color="darkblue", fill="lightblue")
+    show(p3)
+    
+    p3 <- ggplot(result_df,aes(x=bias_2)) + geom_density(color="darkblue", fill="lightblue")
+    show(p3)
+    
+    fit.norm <- fitdist(result_df$differences_1, "norm")
+    plot(fit.norm)
+    
+    fit.norm <- fitdist(result_df$differences_2, "norm")
+    plot(fit.norm)
+    
+    summary(fit.norm)
+  }
+  
+}
 
+#Univariate
 
-test_df <- uni_sim(1000, c(100,100), c(75, 100), c(10, 10), 0.5, 10)
-
-Sig <- (matrix(.5, nrow=2, ncol=2) + diag(2)*.5) * 10
-temp <- mvrnorm(n = 100, mu = c(100, 100), Sigma = Sig)
-
-
-
-
+result_df1 <- uni_sim(10000, c(100,100), c(75, 100), c(10, 10), 0.5, 10)
+result_df2 <- uni_sim(10000, c(100,100), c(75, 100), c(10, 10), 0.5, 5)
+result_df3 <- uni_sim(10000, c(500,500), c(75, 100), c(10, 10), 0.5, 10)
+result_df4 <- uni_sim(10000, c(500,500), c(75, 100), c(10, 10), 0.5, 5)
+result_df5 <- uni_sim(10000, c(100,100), c(75, 100), c(10, 10), 1, 10)
+result_df6 <- uni_sim(10000, c(100,100), c(75, 100), c(10, 10), 1, 5)
+result_df7 <- uni_sim(10000, c(500,500), c(75, 100), c(10, 10), 1, 10)
+result_df8 <- uni_sim(10000, c(500,500), c(75, 100), c(10, 10), 1, 5)
 plot_results(test_df)
 
 
+#Bivariate
+bi_result_df1 <- uni_sim(10000, c(100,100), c(75, 100, 100), c(10, 10, 10), 0.5, 10, 0.5)
+bi_result_df1 <- uni_sim(10000, c(100,100), c(75, 100, 100), c(10, 10, 10), 0.5, 5, 0.5)
+bi_result_df1 <- uni_sim(10000, c(500,500), c(75, 100, 100), c(10, 10, 10), 0.5, 10, 0.5)
+bi_result_df1 <- uni_sim(10000, c(500,500), c(75, 100, 100), c(10, 10, 10), 0.5, 5, 0.5)
+bi_result_df1 <- uni_sim(10000, c(100,100), c(75, 100, 100), c(10, 10, 10), 1, 10, 0.5)
+bi_result_df1 <- uni_sim(10000, c(100,100), c(75, 100, 100), c(10, 10, 10), 1, 5, 0.5)
+bi_result_df1 <- uni_sim(10000, c(500,500), c(75, 100, 100), c(10, 10, 10), 1, 10, 0.5)
+bi_result_df1 <- uni_sim(10000, c(500,500), c(75, 100, 100), c(10, 10, 10), 1, 5, 0.5)
 
